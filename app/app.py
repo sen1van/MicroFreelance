b94f13b28e032a4b57f8c62c420c076e957f640e
Dmitry Leontiev <80615886+sen1van@users.noreply.github.com>
2024-01-14 03:25:27 +0700

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- app/app.py
+++ app/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from random import randint
+from flask_login import LoginManager
+from conifg import Config
+
 
 app = Flask(__name__, static_url_path='/static')
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config.from_object(Config)
 
-@app.route('/')
-def index():
-    return render_template('about.html')
+import routes
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.3.13')
+    app.run(debug=True, host='0.0.0.0')
+
