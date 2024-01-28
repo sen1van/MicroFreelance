b85c3fb6129ad08ecdfd5b2fe66acbcc7b5bb822
Dmitry Leontiev <80615886+sen1van@users.noreply.github.com>
2024-01-28 19:56:25 +0300

    Tangled in 3 pines
    Changed css framework, idk why...

--- app/app.py
+++ app/app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
 from random import randint
-from flask_login import LoginManager
 from conifg import Config
 
 
 app = Flask(__name__, static_url_path='/static')
-login_manager = LoginManager()
-login_manager.init_app(app)
 app.config.from_object(Config)
 
-import routes
+@app.route('/')
+def index():
+    return render_template('base/base.html')
+
+@app.route('/login/')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
