2b94c3d39332a76e4624bbe8090e37562560f618
sen1van <dmitryleontiev16@gmail.com>
2024-02-03 18:08:07 +0700

    More ui

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from random import randint
 from conifg import Config
-
+import fish
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
@@ -14,6 +14,16 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/my-tasks/<filter>')
+@app.route('/my-tasks/')
+@app.route('/my-tasks')
+def my_tasks(filter = None):
+    buff = fish.tasks
+    if filter == 'from-me':
+        buff = [buff[0]]
+    return render_template('my_tasks.html', tasks = buff, me = fish.me)
 
 
 if __name__ == '__main__':
