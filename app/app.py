c5d8c9fb51da32343fc38f3ede110a1f6fd0a25b
Dmitry Leontiev <80615886+sen1van@users.noreply.github.com>
2024-02-08 07:55:21 +0700

    ?

--- app/app.py
+++ app/app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
-from random import randint
-from conifg import Config
 import fish
 
 app = Flask(__name__, static_url_path='/static')
-app.config.from_object(Config)
 
 @app.route('/')
 def index():
@@ -25,6 +22,9 @@ def my_tasks(filter = None):
         buff = [buff[0]]
     return render_template('my_tasks.html', tasks = buff, me = fish.me)
 
+
+@app.route('/profile')
+@app.route('/profile/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
