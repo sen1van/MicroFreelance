b49c053f959a82e3a89411bd04ac9a3ea3294dd1
Dmitry Leontiev <80615886+sen1van@users.noreply.github.com>
2024-02-12 19:52:02 +0700

    add forms

--- app/app.py
+++ app/app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+
+from config import Config
+from forms import LoginForm
+
 import fish
 
 app = Flask(__name__, static_url_path='/static')
+app.config.from_object(Config)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('base/base.html')
 
-@app.route('/login/')
-@app.route('/login')
+@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    error = 'None'
+    if form.validate_on_submit():
+        error='Не правильный логин или пароль'
+    return render_template('login.html', form=form, error=error)
 
 
 @app.route('/my-tasks/<filter>')
@@ -22,9 +32,6 @@ def my_tasks(filter = None):
         buff = [buff[0]]
     return render_template('my_tasks.html', tasks = buff, me = fish.me)
 
-
-@app.route('/profile')
-@app.route('/profile/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
