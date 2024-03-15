bdd70d47d5d46c8245123c84c97d89cbc1b7a2d5
Dmitry Leontiev <80615886+sen1van@users.noreply.github.com>
2024-03-15 23:17:24 +0700

    15.03.2024

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import render_template, flash, redirect, url_for, request
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -8,12 +7,14 @@ from forms import LoginForm
 
 import fish
 
-from models import User
 
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+from models import User
+
 
 @app.route('/')
 @app.route('/index')
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 from app import db
+
 
 
 class User(db.Model):
